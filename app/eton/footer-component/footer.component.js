c6ecc43404448ea3a15849f4fda3f357e5e128ff
Sylvestre Gug <sylvestre.gug@gmail.com>
2017-02-22 11:53:15 -0500

    updated dropdown with proper transclusion for dropdown component, specs and styles edit

--- app/eton/footer-component/footer.component.js
+++ app/eton/footer-component/footer.component.js
@@ -15,16 +15,24 @@
     /* @ngInject */
     function footerComponentController() {
         var vm = this;
+	    vm.listOfCountries = [{name:'sweden',code:'se'},
+		    {name:'france',code:'fr'},
+		    {name:'greece',code:'gr'},
+		    {name:'germany',code:'ge'}];
 
-        vm.$onInit = onInit;
+	    vm.$onInit = onInit;
+	    vm.updateCountry = updateCountry;
 
         ////////////////////
 
         function onInit(){
+	        vm.selectedCountry = vm.listOfCountries[0];
+        }
 
+        function updateCountry(country){
+	        vm.selectedCountry = country;
         }
     }
-
 })();
 
 
--- app/eton/footer-component/footer.component.specs.js
+++ app/eton/footer-component/footer.component.specs.js
@@ -6,11 +6,40 @@ describe("Component: Footer", function () {
 	beforeEach(inject(function(_$componentController_){
 		$componentController = _$componentController_;
 		bindings = {
+
 		};
 		vm = $componentController('footerComponent', null, bindings);
 	}));
 
+	it("should have a list of countries avalable", function () {
+		expect(vm.listOfCountries.length).toEqual(4)
+	});
+
 	it("should have an $onInit function", function () {
 		expect(vm.$onInit).toEqual(jasmine.any(Function))
+	});
+
+	describe("Function: $onInit", function () {
+		beforeEach(function(){
+			vm.$onInit();
+		});
+
+		it("should set the selected country to the first value of the country list", function () {
+			expect(vm.selectedCountry).toEqual(vm.listOfCountries[0]);
+		});
+	});
+
+	it("should have a function to handle country update", function () {
+		expect(vm.updateCountry).toEqual(jasmine.any(Function));
+	});
+
+	describe("Function: updateCountry", function () {
+		beforeEach(function(){
+			vm.updateCountry(vm.listOfCountries[2]);
+		});
+
+		it("should assign the updated country to the selected one", function () {
+			expect(vm.selectedCountry).toEqual(vm.listOfCountries[2]);
+		});
 	});
 });
