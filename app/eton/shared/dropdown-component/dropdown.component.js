8fb87d95885bba0bf41d1e9d38dba42508d9ce08
Sylvestre Gug <sylvestre.gug@gmail.com>
2017-02-22 11:52:34 -0500

    dropdown-component to use transclusion

--- app/eton/shared/dropdown-component/dropdown.component.js
+++ app/eton/shared/dropdown-component/dropdown.component.js
@@ -6,9 +6,14 @@
         .component('dropdownComponent', {
             controller: dropdownComponentController,
             controllerAs: 'vm',
+	        transclude: {
+		        item:'ddItem',
+		        menu:'ddMenu',
+	        },
             templateUrl : 'shared/dropdown-component/dropdownComponentView.html',
             bindings: {
             	list:'<',
+	            item:'<',
 	            onSelect:'&'
             },
         });
@@ -18,16 +23,19 @@
     /* @ngInject */
     function dropdownComponentController() {
         var vm = this;
+	    vm.showMenu = false;
 
         vm.$onInit = onInit;
         vm.onSelectEvent = onSelectEvent;
 
         ////////////////////
-
         function onInit(){
-
+        	// Select the first item of the list by default if item is not provided
+	        (!vm.item) ? vm.item = vm.list[0] : false;
         }
+
         function onSelectEvent(item){
+	        vm.item = item;
         	vm.onSelect({item: item});
         }
     }
--- app/eton/shared/dropdown-component/dropdown.component.specs.js
+++ app/eton/shared/dropdown-component/dropdown.component.specs.js
@@ -31,6 +31,7 @@ describe("Component: Dropdown", function () {
 		};
 
 		bindings = {
+			item: itemData,
 			list: listData,
 			onSelect: onSelectSpy
 		};
@@ -42,17 +43,35 @@ describe("Component: Dropdown", function () {
 		expect(vm.$onInit).toEqual(jasmine.any(Function))
 	});
 
-	it("should expose a vm.closet value inherited from parent binding", function () {
+	describe("Function: onInit",function(){
+		it("should select the first item on the list if vm.item is undefined", function () {
+			vm.item = undefined;
+			vm.$onInit();
+			expect(vm.item).toEqual(listData[0]);
+		});
+
+		it("should NOT select the first item on the list if vm.item is undefined", function () {
+			vm.item = {name:'testing'};
+			vm.$onInit();
+			expect(vm.item).toEqual({name:'testing'});
+		});
+	})
+
+	it("should expose a vm.list value inherited from parent binding", function () {
 		expect(vm.list).toEqual(listData);
 	});
 
-	describe("Function: onClickEvent", function () {
+	describe("Function: onSelectEvent", function () {
 		beforeEach(inject(function(){
 			vm.onSelectEvent(itemData);
 		}));
 
 		it("should fire an event back to parent onClick", function () {
 			expect(onSelectSpy).toHaveBeenCalledWith({item: itemData});
+		});
+
+		it("should assign the item value to the selected item", function () {
+			expect(vm.item).toEqual(itemData);
 		});
 	});
 });
