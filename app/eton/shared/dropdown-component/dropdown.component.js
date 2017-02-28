01f5ef1cb6f02ad63464f9cec8ca9656a4c11f5f
Sylvestre Gug <sylvestre.gug@gmail.com>
2017-02-28 14:54:36 -0500

    dropdown command via bootstrap

--- app/eton/shared/dropdown-component/dropdown.component.js
+++ app/eton/shared/dropdown-component/dropdown.component.js
@@ -18,40 +18,27 @@
             },
         });
 
-	dropdownComponentController.$inject = ['$window'];
+	dropdownComponentController.$inject = ['$window','$document'];
 
     /* @ngInject */
-    function dropdownComponentController($window) {
+    function dropdownComponentController($window,$document) {
         var vm = this;
 	    vm.showMenu = false;
 
         vm.$onInit = onInit;
         vm.onSelectEvent = onSelectEvent;
-        vm.$onDestroy = onDestroy;
 
         ////////////////////
         function onInit(){
         	// Select the first item of the list by default if item is not provided
 	        (!vm.item) ? vm.item = vm.list[0] : false;
-	        $window.addEventListener('click', hideMenu.bind(vm), false);
         }
 
         function onSelectEvent(item){
 	        vm.item = item;
         	vm.onSelect({item: item});
-	        vm.showMenu = false;
         }
-
-	    function hideMenu(e){
-		    (this.showMenu) ? this.showMenu = false : false;
-	    }
-
-	    function onDestroy(){
-		    $window.removeEventListener('click', hideMenu, false);
-	    }
-
     }
-
 })();
 
 
