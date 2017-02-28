d8e437017f3ca1e48461eb3357beede0977c0572
Sylvestre Gug <sylvestre.gug@gmail.com>
2017-02-27 23:41:31 -0500

    dropdown component styles

--- app/eton/shared/dropdown-component/dropdown.component.js
+++ app/eton/shared/dropdown-component/dropdown.component.js
@@ -18,26 +18,38 @@
             },
         });
 
-	dropdownComponentController.$inject = [];
+	dropdownComponentController.$inject = ['$window'];
 
     /* @ngInject */
-    function dropdownComponentController() {
+    function dropdownComponentController($window) {
         var vm = this;
 	    vm.showMenu = false;
 
         vm.$onInit = onInit;
         vm.onSelectEvent = onSelectEvent;
+        vm.$onDestroy = onDestroy;
 
         ////////////////////
         function onInit(){
         	// Select the first item of the list by default if item is not provided
 	        (!vm.item) ? vm.item = vm.list[0] : false;
+	        $window.addEventListener('click', hideMenu.bind(vm), false);
         }
 
         function onSelectEvent(item){
 	        vm.item = item;
         	vm.onSelect({item: item});
+	        vm.showMenu = false;
         }
+
+	    function hideMenu(e){
+		    (this.showMenu) ? this.showMenu = false : false;
+	    }
+
+	    function onDestroy(){
+		    $window.removeEventListener('click', hideMenu, false);
+	    }
+
     }
 
 })();
